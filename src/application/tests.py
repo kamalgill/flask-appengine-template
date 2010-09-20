14f476d918db46ede20b5d810ba3c5d3a97708d7
Kamal Gill <kamalgill@mac.com>
2010-09-19 21:43:02 -0700

    Add placeholder for unit tests

---
+++ src/application/tests.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+tests.py
+
+"""
+
+import unittest
+
+
+class tests(unittest.TestCase):
+    def setUp(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
