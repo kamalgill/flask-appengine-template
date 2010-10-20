aa5eb0bb31a440cf4f0795ff55a6b619d5325c7c
Kamal Gill <kamalgill@mac.com>
2010-10-20 13:49:45 -0700

    PEP 8 compliance

--- src/application/models.py
+++ src/application/models.py
@@ -11,7 +11,7 @@ from google.appengine.ext import db
 
 class ExampleModel(db.Model):
     """Example Model"""
-    example_id = db.StringProperty(required = True)
-    example_title = db.StringProperty(required = True)
+    example_id = db.StringProperty(required=True)
+    example_title = db.StringProperty(required=True)
     added_by = db.UserProperty()
-    timestamp = db.DateTimeProperty(auto_now_add = True)
+    timestamp = db.DateTimeProperty(auto_now_add=True)
