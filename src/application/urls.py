2fe7d8712aef3e4da3d20333cd0a76ebfb7a696e
Kamal Gill <kamalgill@mac.com>
2010-10-28 10:35:31 -0700

    Be explicit when defining URL endpoints

--- src/application/urls.py
+++ src/application/urls.py
@@ -13,13 +13,13 @@ from application import views
 
 ## URL dispatch rules
 # Home page
-app.add_url_rule('/', view_func=views.home)
+app.add_url_rule('/', 'home', view_func=views.home)
 # Say hello
-app.add_url_rule('/hello/<username>', view_func=views.say_hello)
+app.add_url_rule('/hello/<username>', 'say_hello', view_func=views.say_hello)
 # Examples list page
-app.add_url_rule('/examples', view_func=views.list_examples)
+app.add_url_rule('/examples', 'list_examples', view_func=views.list_examples)
 # Add new example via web form
-app.add_url_rule('/example/new', view_func=views.new_example, methods=['GET', 'POST'])
+app.add_url_rule('/example/new', 'new_example', view_func=views.new_example, methods=['GET', 'POST'])
 
 
 ## Error handlers
