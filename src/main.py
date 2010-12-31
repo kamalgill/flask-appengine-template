ca47d850e2e813cc5334a6b62647f08bfb6a3bca
Stavros Korokithakis <stavros@korokithakis.net>
2011-01-01 01:14:51 +0200

    Moved packages to zips in the "packages" directory.

--- src/main.py
+++ src/main.py
@@ -5,6 +5,11 @@ Primary App Engine app handler
 
 """
 
+import sys, os
+for filename in os.listdir("packages"):
+    if filename.endswith(".zip"):
+        sys.path.insert(0, "packages/%s" % filename)
+
 from google.appengine.ext.webapp.util import run_wsgi_app
 from application import app
 
