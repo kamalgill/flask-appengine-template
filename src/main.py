479cf11095ec0103f1c9d4c2d31b8f145999ea28
Stavros Korokithakis <stavros@stochastictechnologies.com>
2011-01-09 21:01:02 +0200

    Made the directory name a variable.

--- src/main.py
+++ src/main.py
@@ -6,9 +6,12 @@ Primary App Engine app handler
 """
 
 import sys, os
-for filename in os.listdir("packages"):
+
+package_dir = "packages"
+
+for filename in os.listdir(package_dir):
     if filename.endswith((".zip", ".egg")):
-        sys.path.insert(0, "packages/%s" % filename)
+        sys.path.insert(0, "%s/%s" % (package_dir, filename))
 
 from google.appengine.ext.webapp.util import run_wsgi_app
 from application import app
