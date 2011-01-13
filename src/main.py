169470d3a1ec3f35cfe96227af6de3498eca96c4
Kamal Gill <kamalgill@mac.com>
2011-01-13 15:52:27 -0800

    Keep Werkzeug as an unzipped package, as it doesn't seem to play nicely with zipimport on App Engine

--- src/main.py
+++ src/main.py
@@ -9,6 +9,10 @@ import sys, os
 
 package_dir = "packages"
 
+# Allow unzipped packages to be imported
+# from packages folder
+sys.path.insert(0, package_dir)
+
 # Append zip archives to path for zipimport
 for filename in os.listdir(package_dir):
     if filename.endswith((".zip", ".egg")):
