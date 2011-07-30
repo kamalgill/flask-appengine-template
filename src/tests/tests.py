6e4a49eab3d34a81272592b25803435cb5de5146
Juarez Bochi <jbochi@gmail.com>
2011-07-30 09:37:08 -0300

    Fixed tests (added memcache support)

--- src/tests/tests.py
+++ src/tests/tests.py
@@ -23,6 +23,7 @@ class DemoTestCase(unittest.TestCase):
         self.testbed.activate()
         self.testbed.init_datastore_v3_stub()
         self.testbed.init_user_stub()
+        self.testbed.init_memcache_stub()
 
     def tearDown(self):
         self.testbed.deactivate()
