8b1c3e35774eb730c44f3a3674cf04f8b3a965a9
Irving Popovetsky <irving@popovetsky.com>
2013-01-05 16:27:34 -0800

    Enable jinja2 loopcontrols extension
    from here: http://jinja.pocoo.org/docs/extensions/#loop-controls

--- src/application/__init__.py
+++ src/application/__init__.py
@@ -10,6 +10,9 @@ from flaskext.gae_mini_profiler import GAEMiniProfiler
 app = Flask('application')
 app.config.from_object('application.settings')
 
+# Enable jinja2 loop controls extension
+app.jinja_env.add_extension('jinja2.ext.loopcontrols')
+
 # Enable profiler (enabled in non-production environ only)
 GAEMiniProfiler(app)
 
