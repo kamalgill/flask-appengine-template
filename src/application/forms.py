8ddfbf2d8fdd66fc74d13d1918b2fcdb3a390b58
Kamal Gill <kamalgill@mac.com>
2013-04-20 17:13:32 -0700

    Update WTForms to latest tip from bitbucket to enable NDB ModelForm support. Fixes #35

--- src/application/forms.py
+++ src/application/forms.py
@@ -10,8 +10,17 @@ See: http://flask.pocoo.org/docs/patterns/wtforms/
 
 from flaskext import wtf
 from flaskext.wtf import validators
+from wtforms.ext.appengine.ndb import model_form
+
+from .models import ExampleModel
 
 
-class ExampleForm(wtf.Form):
+class ClassicExampleForm(wtf.Form):
     example_name = wtf.TextField('Name', validators=[validators.Required()])
     example_description = wtf.TextAreaField('Description', validators=[validators.Required()])
+
+# App Engine ndb model form example
+ExampleForm = model_form(ExampleModel, wtf.Form, field_args={
+    'example_name': dict(validators=[validators.Required()]),
+    'example_description': dict(validators=[validators.Required()]),
+})
