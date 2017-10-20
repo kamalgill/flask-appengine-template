cb7155c768e0d1604aeb6813da536a6ff84db2ed
Kamal Gill <kamal@kamalgill.net>
2017-10-20 15:37:15 -0700

    Refactor wtforms and flask_wtf imports to avoid deprecated flaskext.wtf notation

--- src/application/forms.py
+++ src/application/forms.py
@@ -8,19 +8,20 @@ See: http://flask.pocoo.org/docs/patterns/wtforms/
 
 """
 
-from flaskext import wtf
-from flaskext.wtf import validators
+from flask_wtf import form
+from wtforms import fields, validators
 from wtforms.ext.appengine.ndb import model_form
 
 from models import ExampleModel
 
 
-class ClassicExampleForm(wtf.Form):
-    example_name = wtf.TextField('Name', validators=[validators.Required()])
-    example_description = wtf.TextAreaField('Description', validators=[validators.Required()])
+class ClassicExampleForm(form.FlaskForm):
+    example_name = fields.StringField('Name', validators=[validators.DataRequired()])
+    example_description = fields.TextAreaField('Description', validators=[validators.DataRequired()])
+
 
 # App Engine ndb model form example
-ExampleForm = model_form(ExampleModel, wtf.Form, field_args={
-    'example_name': dict(validators=[validators.Required()]),
-    'example_description': dict(validators=[validators.Required()]),
+ExampleForm = model_form(ExampleModel, form.FlaskForm, field_args={
+    'example_name': dict(validators=[validators.DataRequired()]),
+    'example_description': dict(validators=[validators.DataRequired()]),
 })
